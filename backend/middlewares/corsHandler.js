0e5370f4578c4b5adc6cf4e5af65b22563760874
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-15 14:52:34 +0300

    fix: добавить локалхост в список разрешенных для cors

--- backend/middlewares/corsHandler.js
+++ backend/middlewares/corsHandler.js
@@ -2,7 +2,8 @@
 const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
-  'localhost:3000',
+  'http://localhost:3000',
+  'https://localhost:3000',
   'https://evg.mesto.nomoredomains.icu',
   'http://evg.mesto.nomoredomains.icu',
 ];
